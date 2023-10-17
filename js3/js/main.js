e8887905c10f1e92f4a6045d7e94a57357abf164
danfranc0 <dfranco1@ufl.edu>
2023-10-16 23:59:20 -0400

    js3

---
+++ js3/js/main.js
@@ -0,0 +1,10 @@
+// strings
+const myVariable = "Mathematic";
+
+console.log(myVariable.charAt(5));
+console.log(myVariable.slice(5, 8));
+
+
+console.log(myVariable.split("e"));
+
+console.log("John,Joe,Dave".split(","));
