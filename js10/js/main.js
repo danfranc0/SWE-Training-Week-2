620253eeea640111ae8ea575e92ff5bc51d489ec
danfranc0 <dfranco1@ufl.edu>
2023-11-06 00:47:34 -0500

    ch 10 & 11

---
+++ js10/js/main.js
@@ -0,0 +1,11 @@
+// user input
+
+let name = prompt("Please enter your name.");
+
+if(name) {
+    console.log(name.length);
+    console.log(name.trim().length);
+    console.log(name.trim());
+} else {
+    console.log("You didn't enter your name.")
+}
